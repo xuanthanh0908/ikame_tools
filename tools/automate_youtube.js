7a104073c18e0dce9d59115d737fe46d578c6523
xuanthanh0908 <hd13112011@gmail.com>
2023-05-08 18:26:33 +0700

    update

--- tools/automate_youtube.js
+++ tools/automate_youtube.js
@@ -9,14 +9,13 @@ const { emitEvent } = require("../utils/socket");
 const fs = require("fs");
 // const backend_campaign_url = "https://api.ikamegroup.com/api/v1";
 const backend_campaign_url = "http://localhost:9000/api/v1";
-const url = {
-  ADSGROUP: "/ads-asset",
-};
+// const url = {
+//   ADSGROUP: "/ads-asset",
+// };
 const DATA = {
   video_path: "C:\\Users\\hd131\\Downloads\\Video",
-  title: ["Title 01", "Title 02"],
-  desc: ["Description 01", "Description 02"],
   playlist: "Test 03",
+  channel: "nunmoon",
 };
 /// clear input
 const clearInput = async (el) => {
@@ -29,8 +28,6 @@ const run_Now = () => {
   //   const DATA = req.data
   // console.log("=============DATA==============", DATA);
   const max_time = 70000;
-  let posX = 0;
-  let posY = 0;
   return new Promise(async (resolve, reject) => {
     readFile()
       .then(async (path) => {
@@ -46,28 +43,48 @@ const run_Now = () => {
         driver.manage().window().maximize();
         try {
           await driver.get("https://studio.youtube.com");
-          //   await driver.manage().window().setRect({
-          //     width: 960,
-          //     height: 1080,
-          //     x: posX,
-          //     y: posY,
-          //   })
-
-          // wait for button upload video showing
-          const btn_upload_path = "//ytcp-icon-button[@id='upload-icon']";
-          const condition = until.elementLocated({
-            xpath: btn_upload_path,
-          });
-          await driver.wait(condition, max_time).then(async () => {
-            await driver
-              .findElement(By.xpath(btn_upload_path))
-              .click()
-              .then(async () => {
-                await handeleStep_02(driver)
-                  .then(() => resolve("Run test successfully"))
-                  .catch(reject);
-              });
-          });
+          /// change channel
+          const chanel_path = "//button[@id='avatar-btn']";
+          await driver
+            .findElement(By.xpath(chanel_path))
+            .click()
+            .then(async () => {
+              const switch_acc_path =
+                "//body//ytcp-app//ytd-compact-link-renderer[3]";
+              await driver
+                .findElement(By.xpath(switch_acc_path))
+                .click()
+                .then(async () => {
+                  await driver.sleep(500).then(async () => {
+                    const all_acc_tagname = "ytd-account-item-renderer";
+                    await driver
+                      .findElements(By.tagName(all_acc_tagname))
+                      .then(async (elements) => {
+                        for (let index = 0; index < elements.length; index++) {
+                          const element = elements[index];
+                          let pathItem =
+                            '(//yt-formatted-string[@id="channel-title"])[' +
+                            (index + 1) +
+                            "]";
+                          driver
+                            .findElement(By.xpath(pathItem))
+                            .getText()
+                            .then(async (text) => {
+                              if (text === DATA.channel) {
+                                await element.click().then(async () => {
+                                  await handeleStep_02(driver)
+                                    .then(() =>
+                                      resolve("Run test successfully")
+                                    )
+                                    .catch(reject);
+                                });
+                              }
+                            });
+                        }
+                      });
+                  });
+                });
+            });
         } finally {
           //
           await driver.sleep(2000);
@@ -80,65 +97,124 @@ const run_Now = () => {
       });
   });
 };
-// handle upload video
+// handle read video path && run consequent
 const handeleStep_02 = async (driver) => {
+  // Upload
   return new Promise(async (resolve, reject) => {
     try {
-      // Upload
       const files = fs.readdirSync(DATA.video_path);
+      const title = files.map((file) => file);
       const filePaths = files.map((file) => `${DATA.video_path}\\${file}`);
-      const input_upload_path = "//input[@name='Filedata']";
-      await driver
-        .findElement(By.xpath(input_upload_path))
-        .sendKeys(filePaths[0]);
-      const input_title_path = "(//div[@id='textbox'])[1]";
-      const condition_02 = until.elementLocated({
-        xpath: input_title_path,
-      });
-      await driver.wait(condition_02, max_time).then(async () => {
-        /// handle title input
-        const title = await driver.findElement(By.xpath(input_title_path));
-        await clearInput(title);
-        await title.sendKeys(DATA.title[0]);
-
-        /// handle desc input
-        const desc_input_path = "(//div[@id='textbox'])[2]";
-        const desc = await driver.findElement(By.xpath(desc_input_path));
-        await clearInput(desc);
-        await desc.sendKeys(DATA.desc[0]);
-        // handle choose playlist
-        const playlist_path =
-          "(//div[@class='right-container style-scope ytcp-dropdown-trigger'])[1]";
-        const playlist = await driver.findElement(By.xpath(playlist_path));
-        await playlist.click().then(async () => {
-          const find_playlist_name =
-            "//span[contains(text(),'" + DATA.playlist + "')]";
-          const condition_03 = until.elementLocated({
-            xpath: find_playlist_name,
-          });
-          await driver.wait(condition_03, max_time).then(async () => {
-            const playlist_checked = await driver.findElement(
-              By.xpath(find_playlist_name)
-            );
-            await driver.executeScript(
-              "arguments[0].click()",
-              playlist_checked
-            );
-            // close playlist
-            const btn_done_path = "//div[normalize-space()='Done']";
-            await driver.findElement(By.xpath(btn_done_path)).click();
-          });
-        });
-        // handle choose options - for children
-        const options_path = "(//div[@id='radioLabel'])[2]";
-        await driver.findElement(By.xpath(options_path)).click();
-        // handle next button
-        const btn_next_path = "//ytcp-button[@id='next-button']";
-        await driver
-          .findElement(By.xpath(btn_next_path))
-          .click()
+      for (const [index, filePath] of filePaths.entries()) {
+        await handeleStep_03(driver, filePath, title[index])
           .then(() => resolve("Run test successfully"))
           .catch(reject);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+// handle upload video
+const handeleStep_03 = async (driver, file_path, title_) => {
+  const max_time = 40000;
+  return new Promise(async (resolve, reject) => {
+    try {
+      // wait for button upload video showing
+      const btn_upload_path = "//ytcp-icon-button[@id='upload-icon']";
+      const condition = until.elementLocated({
+        xpath: btn_upload_path,
+      });
+      await driver.wait(condition, max_time).then(async () => {
+        await driver
+          .findElement(By.xpath(btn_upload_path))
+          .click()
+          .then(async () => {
+            const input_upload_path = "//input[@name='Filedata']";
+            await driver
+              .findElement(By.xpath(input_upload_path))
+              .sendKeys(file_path);
+            const input_title_path = "(//div[@id='textbox'])[1]";
+            const condition_02 = until.elementLocated({
+              xpath: input_title_path,
+            });
+            await driver.wait(condition_02, max_time).then(async () => {
+              /// handle title input
+              const title = await driver.findElement(
+                By.xpath(input_title_path)
+              );
+              await clearInput(title);
+              await title.sendKeys(title_);
+
+              /// handle desc input
+              // const desc_input_path = "(//div[@id='textbox'])[2]";
+              // const desc = await driver.findElement(By.xpath(desc_input_path));
+              // await clearInput(desc);
+              // await desc.sendKeys(DATA.desc[index]);
+              // handle choose playlist
+              const playlist_path =
+                "(//div[@class='right-container style-scope ytcp-dropdown-trigger'])[1]";
+              const playlist = await driver.findElement(
+                By.xpath(playlist_path)
+              );
+              await playlist.click().then(async () => {
+                const find_playlist_name =
+                  "//span[contains(text(),'" + DATA.playlist + "')]";
+                const condition_03 = until.elementLocated({
+                  xpath: find_playlist_name,
+                });
+                await driver.wait(condition_03, max_time).then(async () => {
+                  const playlist_checked = await driver.findElement(
+                    By.xpath(find_playlist_name)
+                  );
+                  await driver.executeScript(
+                    "arguments[0].click()",
+                    playlist_checked
+                  );
+                  // close playlist
+                  const btn_done_path = "//div[normalize-space()='Done']";
+                  await driver.findElement(By.xpath(btn_done_path)).click();
+                });
+              });
+              // handle choose options - for children
+              const options_path = "(//div[@id='radioLabel'])[2]";
+              await driver.findElement(By.xpath(options_path)).click();
+              // handle next button
+              const btn_next_path = "//ytcp-button[@id='next-button']";
+
+              for (let i = 0; i < 3; i++) {
+                await driver.sleep(2000).then(async () => {
+                  await driver.findElement(By.xpath(btn_next_path)).click();
+                });
+              }
+              const save_or_pb_unlisted_path =
+                "//tp-yt-paper-radio-button[@name='UNLISTED']//div[@id='radioLabel']";
+              const condition_04 = until.elementLocated({
+                xpath: save_or_pb_unlisted_path,
+              });
+              await driver.wait(condition_04, max_time).then(async () => {
+                await driver
+                  .findElement(By.xpath(save_or_pb_unlisted_path))
+                  .click();
+
+                const btn_save_path =
+                  "//div[@class='label style-scope ytcp-button'][normalize-space()='Save']";
+                await driver.findElement(By.xpath(btn_save_path)).click();
+                const btn_close_process_path =
+                  "//ytcp-button[@id='close-button']//div[@class='label style-scope ytcp-button'][normalize-space()='Close']";
+                const condition_05 = until.elementLocated({
+                  xpath: btn_close_process_path,
+                });
+                await driver.wait(condition_05, max_time).then(async () => {
+                  await driver
+                    .findElement(By.xpath(btn_close_process_path))
+                    .click()
+                    .then(() => resolve("Run test successfully"))
+                    .catch(reject);
+                });
+              });
+            });
+          });
       });
     } catch (error) {
       console.log(error);
@@ -146,4 +222,5 @@ const handeleStep_02 = async (driver) => {
     }
   });
 };
-run_Now();
+
+// run_Now();
