8520814684475e78c822e787e454f33ae20958ad
xuanthanh0908 <hd13112011@gmail.com>
2023-05-16 17:35:24 +0700

    update

--- tools/automate_ggAds.js
+++ tools/automate_ggAds.js
@@ -109,7 +109,7 @@ const runTest = async (req, res, next) => {
                         .sendKeys(DATA.package_name)
                         .then(async () => {
                           const select_app_path =
-                            ".app-info._ngcontent-awn-CM_EDITING-33";
+                            ".leading-text + .input-container .input.input-area";
 
                           const conditions_02 = until.elementLocated({
                             css: select_app_path,
