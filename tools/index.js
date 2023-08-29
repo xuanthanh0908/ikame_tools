bd93efd068f797858f6574204e2f072add9dfd5c
xuanthanh0908 <hd13112011@gmail.com>
2023-08-29 11:10:53 +0700

    update

--- tools/index.js
+++ tools/index.js
@@ -26,3 +26,28 @@ module.exports = {
   handMultiFetchMintegralRemovedCreative,
   handleFetchMintegralRemoveCreative,
 };
+
+[
+  "ngocttb@ikameglobal.com",
+  "cuongpm@ikameglobal.com",
+  "anhht@ikameglobal.com",
+  "vietld@ikameglobal.com",
+  "linhnk@ikameglobal.com",
+  "gamlp@ikameglobal.com",
+  "thuna@ikameglobal.com",
+  "hungnh@ikameglobal.com",
+  "nhungph@ikameglobal.com",
+  "thaont@ikameglobal.com",
+  "thaodth@ikameglobal.com",
+  "loantt@ikameglobal.com",
+  "sontt@ikameglobal.com",
+  "hoangtd@ikameglobal.com",
+  "hungtv@ikameglobal.com",
+  "linhnd@ikameglobal.com",
+  "dinhhv@ikameglobal.com",
+  "anhtq@ikameglobal.com",
+  "huynq@ikameglobal.com",
+  "hieuct@ikameglobal.com",
+  "anhtdt@ikameglobal.com",
+  "anhdqh@ikameglobal.com",
+];
