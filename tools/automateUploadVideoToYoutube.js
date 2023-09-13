0025b3aca304080ff28cfd14d55eb84d4ca5e61a
xuanthanh0908 <hd13112011@gmail.com>
2023-09-13 18:15:30 +0700

    update

--- tools/automateUploadVideoToYoutube.js
+++ tools/automateUploadVideoToYoutube.js
@@ -579,7 +579,7 @@ const handMultiFetchYTB = async () => {
 };
 const scheduleRun = async () => {
   // console.log("CHECKED  CRON JOB RUN");
-  crontab.scheduleJob("*/15 * * * * *", async function () {
+  crontab.scheduleJob("*/45 * * * * *", async function () {
     console.log("====== CRON JOB RUN ======");
     const checkOpened = await checkBrowserIsOpened();
     if (!checkOpened) {
